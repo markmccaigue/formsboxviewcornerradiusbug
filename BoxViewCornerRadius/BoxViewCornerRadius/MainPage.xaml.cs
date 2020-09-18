d6967609f909ad712deb279b5d65d8d241b28fa9
Mark McCaigue <mark.mccaigue@theappbuilder.com>
2020-09-18 17:09:23 +0100

    initial commit

---
+++ BoxViewCornerRadius/BoxViewCornerRadius/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BoxViewCornerRadius
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            Box.CornerRadius = 10;
+
+            base.OnAppearing();
+        }
+    }
+}
